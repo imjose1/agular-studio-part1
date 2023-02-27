d7fcb79eca4c6d581eb7d4592d3af577ace5f081
Jose g hernandez <jose_ghj@hotmail.com>
2023-02-27 13:10:52 -0600

    studio part 1

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  missionName="Back to the future";
+  rocketName="Apollo 2030";
+}
